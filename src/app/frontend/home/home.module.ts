d180db2ed8a125d1c00c07071861fc58deadf924
Jackson Vera <jacksonverapineda@icloud.com>
2024-03-05 14:51:40 -0500

    fix: :bug: hotfix

--- src/app/frontend/home/home.module.ts
+++ src/app/frontend/home/home.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
 import { BannerComponent } from '../components/banner/banner.component';
 import { HomeRoutingModule } from './home.routing';
+import { CarrouselComponent } from '../components/carrousel/carrousel.component';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, BannerComponent],
+  imports: [CommonModule, HomeRoutingModule, BannerComponent, CarrouselComponent],
   exports: [HomeComponent],
 })
 export class HomeModule {}
