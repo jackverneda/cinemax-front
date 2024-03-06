5fa03c6a172a4d6444dfb1a0246a234ae828851c
Roger Fuentes <fuentesroger65@gmail.com>
2024-03-06 15:39:03 -0500

    feat: :sparkles: agg carrousel and promo

---
+++ src/app/frontend/components/carrousel-clasicos/carrousel-clasicos.component.ts
@@ -0,0 +1,41 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { register } from 'swiper/element/bundle';
+import { SwiperOptions } from 'swiper/types/swiper-options';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-carrusel-clasicos',
+  standalone: true,
+  templateUrl: './carrousel-clasicos.component.html',
+  styleUrl: 'carrousel-clasicos.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [MatButtonModule],
+})
+export class CarrouselClasicos {
+  public config;
+  public dataImages: any[];
+
+  constructor() {
+    register();
+    this.config = {
+      slidesPerView: 5,
+      direction: 'horizontal',
+      loop: true,
+      spaceBetween: '30px',
+      navigation: true,
+      pagination: { bulletActiveClass: 'swiper-pagination-bullet-active', clickable: true },
+    };
+    this.dataImages = [
+      { file: '../../../../assets/images/movies/wonka.jpeg' },
+      { file: '../../../../assets/images/movies/thecolorpurplEXP.jpeg' },
+      { file: '../../../../assets/images/movies/wonka.jpeg' },
+      { file: '../../../../assets/images/movies/thecolorpurplEXP.jpeg' },
+      { file: '../../../../assets/images/movies/thecolorpurplEXP.jpeg' },
+      { file: '../../../../assets/images/movies/wonka.jpeg' },
+      { file: '../../../../assets/images/movies/thecolorpurplEXP.jpeg' },
+      { file: '../../../../assets/images/movies/wonka.jpeg' },
+      { file: '../../../../assets/images/movies/thecolorpurplEXP.jpeg' },
+      { file: '../../../../assets/images/movies/thecolorpurplEXP.jpeg' }
+    ];
+  }
+}
--- src/app/frontend/home/home.module.ts
+++ src/app/frontend/home/home.module.ts
@@ -4,10 +4,12 @@ import { HomeComponent } from './home.component';
 import { BannerComponent } from '../components/banner/banner.component';
 import { HomeRoutingModule } from './home.routing';
 import { CarrouselComponent } from '../components/carrousel/carrousel.component';
+import { CarrouselClasicos } from '../components/carrousel-clasicos/carrousel-clasicos.component';
+import { Promo } from '../components/promo/promo.component';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, BannerComponent, CarrouselComponent],
+  imports: [CommonModule, HomeRoutingModule,CarrouselClasicos, Promo, BannerComponent, CarrouselComponent],
   exports: [HomeComponent],
 })
 export class HomeModule {}
