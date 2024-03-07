acc45aa1251ae47fe287d3cc25fe8908730542be
Roger Fuentes <fuentesroger65@gmail.com>
2024-03-07 15:07:12 -0500

    Agg movie-rooms component

--- src/app/frontend/components/carrousel-clasicos/carrousel-clasicos.component.ts
+++ src/app/frontend/components/carrousel-clasicos/carrousel-clasicos.component.ts
@@ -2,6 +2,7 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { register } from 'swiper/element/bundle';
 import { SwiperOptions } from 'swiper/types/swiper-options';
 import { MatButtonModule } from '@angular/material/button';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-carrusel-clasicos',
@@ -10,6 +11,7 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: 'carrousel-clasicos.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [MatButtonModule],
+  
 })
 export class CarrouselClasicos {
   public config;
@@ -24,6 +26,7 @@ export class CarrouselClasicos {
       spaceBetween: '30px',
       navigation: true,
       pagination: { bulletActiveClass: 'swiper-pagination-bullet-active', clickable: true },
+      
     };
     this.dataImages = [
       { file: '../../../../assets/images/movies/wonka.jpeg' },
@@ -38,4 +41,5 @@ export class CarrouselClasicos {
       { file: '../../../../assets/images/movies/thecolorpurplEXP.jpeg' }
     ];
   }
+  
 }
