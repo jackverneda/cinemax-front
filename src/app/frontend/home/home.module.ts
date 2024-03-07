692aea4fb228e1a30b9aa259e0a1e80b19fb3399
Jackson Vera <jacksonverapineda@icloud.com>
2024-03-07 10:37:19 -0500

    feat: :sparkles: carrousel-right

--- src/app/frontend/home/home.module.ts
+++ src/app/frontend/home/home.module.ts
@@ -4,10 +4,11 @@ import { HomeComponent } from './home.component';
 import { BannerComponent } from '../components/banner/banner.component';
 import { HomeRoutingModule } from './home.routing';
 import { CarrouselComponent } from '../components/carrousel/carrousel.component';
+import { CarrouselRightComponent } from '../components/carrousel-right/carrousel-right.component';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, BannerComponent, CarrouselComponent],
+  imports: [CommonModule, HomeRoutingModule, CarrouselRightComponent, BannerComponent, CarrouselComponent],
   exports: [HomeComponent],
 })
 export class HomeModule {}
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -3,12 +3,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   templateUrl: './footer.component.html',
   styleUrl: 'footer.component.scss',
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, RouterModule],
 })
 export class FooterComponent {}
